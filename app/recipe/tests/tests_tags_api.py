dbd16675c8c9f2c883c2ed8f17ce250fb5dbafd4
Ryan Neldner <ryan@wpromote.com>
2020-08-20 17:04:38 -0700

    added create tag feature

--- app/recipe/tests/tests_tags_api.py
+++ app/recipe/tests/tests_tags_api.py
@@ -64,3 +64,21 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """Test creating a new tag"""
+        payload = {'name': 'Test Tag'}
+        self.client.post(TAGS_URLS, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_invalid_tag_name_fails(self):
+        """Test creating a tag with an invalid name fails"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URLS, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
